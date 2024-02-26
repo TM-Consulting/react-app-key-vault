ab0e0d1cc09874b478d4b049431d418462246784
Monaim <monaim@winnersclubsaadben.onmicrosoft.com>
2024-02-26 20:39:35 +0100

    sdf

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { useEffect } from "react";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    console.log("envvvv", process.env); // Add this line temporarily
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <p>Name: {process.env.REACT_APP_TEST_KEY}</p>
+        <p>public: {process.env.PUBLIC_URL}</p>
       </header>
     </div>
   );
